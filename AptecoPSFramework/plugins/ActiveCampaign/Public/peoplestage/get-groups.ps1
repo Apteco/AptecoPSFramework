ca62ae5f7b2badfe9a4dfc3a735ba9e272ec122a
FlorianSf <florian.von.bracht@apteco.de>
2024-05-27 23:52:50 +0200

    Remove not needed class (is already inherited) and adding first PS functionalities for #11

---
+++ AptecoPSFramework/plugins/ActiveCampaign/Public/peoplestage/get-groups.ps1
@@ -0,0 +1,113 @@
+﻿
+
+function Get-Groups {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$false)][Hashtable] $InputHashtable
+        #[Parameter(Mandatory=$false)][Switch] $DebugMode = $false
+    )
+
+    begin {
+
+
+        #-----------------------------------------------
+        # LOG
+        #-----------------------------------------------
+
+        $moduleName = "GETGROUPS"
+
+        # Start the log
+        Write-Log -message $Script:logDivider
+        Write-Log -message $moduleName -Severity INFO
+
+        # Log the params, if existing
+        Write-Log -message "INPUT:"
+        if ( $InputHashtable ) {
+            $InputHashtable.Keys | ForEach-Object {
+                $param = $_
+                Write-Log -message "    $( $param ) = '$( $InputHashtable[$param] )'" -writeToHostToo $false
+            }
+        }
+
+        #-----------------------------------------------
+        # DEPENDENCIES
+        #-----------------------------------------------
+
+        #...
+
+    }
+
+    process {
+
+        #-----------------------------------------------
+        # LOAD MAILINGS
+        #-----------------------------------------------
+
+        $groups = get-list | select id, name
+
+        <#
+        [void]$mailings.Add(
+            [PSCustomObject]@{
+                "id" = "a"
+                "name" = "add"
+            }
+        )
+        [void]$mailings.Add(
+            [PSCustomObject]@{
+                "id" = "r"
+                "name" = "remove"
+            }
+        )
+        #>
+
+        # Load and filter list into array of mailings objects
+        $groupsList = [System.Collections.ArrayList]@()
+        $groups | ForEach-Object {
+            $group = $_
+            [void]$groupsList.add(
+                [MailingList]@{
+                    mailingListId=$group.id
+                    mailingListName=$group.name
+                }
+            )
+        }
+
+        # Transform the mailings array into the needed output format
+        $columns = @(
+            @{
+                name="id"
+                expression={ $_.mailingListId }
+            }
+            @{
+                name="name"
+                expression={ $_.toString() }
+            }
+        )
+
+        $lists = [System.Collections.ArrayList]@()
+        [void]$lists.AddRange(@( $groupsList | Select-Object $columns ))
+
+        If ( $lists.count -gt 0 ) {
+
+            Write-Log "Loaded $( $lists.Count ) lists/groups" -severity INFO #-WriteToHostToo $false
+
+        } else {
+
+            $msg = "No lists loaded -> please check!"
+            Write-Log -Message $msg -Severity ERROR
+            throw [System.IO.InvalidDataException] $msg
+
+        }
+
+        # Return
+        $lists
+
+    }
+
+    end {
+
+    }
+
+}
+
+
---
+++ AptecoPSFramework/plugins/ActiveCampaign/Public/peoplestage/get-messages.ps1
@@ -0,0 +1,118 @@
+﻿
+
+function Get-Messages {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$false)][Hashtable] $InputHashtable
+        #[Parameter(Mandatory=$false)][Switch] $DebugMode = $false
+    )
+
+    begin {
+
+
+        #-----------------------------------------------
+        # LOG
+        #-----------------------------------------------
+
+        $moduleName = "GETMESSAGES"
+
+        # Start the log
+        Write-Log -message $Script:logDivider
+        Write-Log -message $moduleName -Severity INFO
+
+        # Log the params, if existing
+        Write-Log -message "INPUT:"
+        if ( $InputHashtable ) {
+            $InputHashtable.Keys | ForEach-Object {
+                $param = $_
+                Write-Log -message "    $( $param ) = '$( $InputHashtable[$param] )'" -writeToHostToo $false
+            }
+        }
+
+        #-----------------------------------------------
+        # DEPENDENCIES
+        #-----------------------------------------------
+
+        #...
+
+    }
+
+    process {
+
+        #-----------------------------------------------
+        # LOAD MAILINGS
+        #-----------------------------------------------
+
+        # TODO Add a switch for tags/events etc.
+
+        $tags = get-tag | select-object id, @{name="name";expression={ $_.tag }}
+
+
+        $mailings = [System.Collections.ArrayList]@()
+        $mailings.AddRange($tags)
+
+        <#
+        [void]$mailings.Add(
+            [PSCustomObject]@{
+                "id" = "a"
+                "name" = "add"
+            }
+        )
+        [void]$mailings.Add(
+            [PSCustomObject]@{
+                "id" = "r"
+                "name" = "remove"
+            }
+        )
+        #>
+
+        # Load and filter list into array of mailings objects
+        $mailingsList = [System.Collections.ArrayList]@()
+        $mailings | ForEach-Object {
+            $mailing = $_
+            [void]$mailingsList.add(
+                [Mailing]@{
+                    mailingId=$mailing.id
+                    mailingName=$mailing.name
+                }
+            )
+        }
+
+        # Transform the mailings array into the needed output format
+        $columns = @(
+            @{
+                name="id"
+                expression={ $_.mailingId }
+            }
+            @{
+                name="name"
+                expression={ $_.toString() }
+            }
+        )
+
+        $messages = [System.Collections.ArrayList]@()
+        [void]$messages.AddRange(@( $mailingsList | Select-Object $columns ))
+
+        If ( $messages.count -gt 0 ) {
+
+            Write-Log "Loaded $( $messages.Count ) messages" -severity INFO #-WriteToHostToo $false
+
+        } else {
+
+            $msg = "No messages loaded -> please check!"
+            Write-Log -Message $msg -Severity ERROR
+            throw [System.IO.InvalidDataException] $msg
+
+        }
+
+        # Return
+        $messages
+
+    }
+
+    end {
+
+    }
+
+}
+
