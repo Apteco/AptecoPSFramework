947d04c955e3f27ef5a1019153cfd053455633a8
FlorianSf <florian.von.bracht@apteco.de>
2024-07-01 12:45:50 +0200

    Adding a peoplestage function to get lists for #13

---
+++ AptecoPSFramework/plugins/InxmailPro/Public/PeopleStage/get-groups.ps1
@@ -0,0 +1,99 @@
+﻿
+
+function Get-Groups {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$false)][Hashtable] $InputHashtable
+        #[Parameter(Mandatory=$false)][Switch] $DebugMode = $false
+    )
+
+    begin {
+
+
+        #-----------------------------------------------
+        # LOG
+        #-----------------------------------------------
+
+        $moduleName = "GETGROUPS"
+
+        # Start the log
+        Write-Log -message $Script:logDivider
+        Write-Log -message $moduleName -Severity INFO
+
+        # Log the params, if existing
+        Write-Log -message "INPUT:"
+        if ( $InputHashtable ) {
+            $InputHashtable.Keys | ForEach-Object {
+                $param = $_
+                Write-Log -message "    $( $param ) = '$( $InputHashtable[$param] )'" -writeToHostToo $false
+            }
+        }
+
+        #-----------------------------------------------
+        # DEPENDENCIES
+        #-----------------------------------------------
+
+        #Import-Module MeasureRows
+        #Import-Module SqlServer
+        #Import-Module ConvertUnixTimestamp
+        #Import-Lib -IgnorePackageStructure
+
+    }
+
+    process {
+
+        # Load mailings data from CleverReach
+        $groups = Get-List -Type STANDARD
+
+        Write-Log "Loaded $( $groups.Count ) groups from Inxmail" -severity INFO
+
+        # Load and filter list into array of mailings objects
+        $groupsList = [System.Collections.ArrayList]@()
+        $groups | ForEach-Object {
+            $group = $_
+            [void]$groupsList.add(
+                [MailingList]@{
+                    mailingListId=$group.id
+                    mailingListName=( $group.name -replace '[^\w\s]', '' )
+                }
+            )
+        }
+
+        # Transform the mailings array into the needed output format
+        $columns = @(
+            @{
+                name="id"
+                expression={ $_.mailingListId }
+            }
+            @{
+                name="name"
+                expression={ $_.toString() }
+            }
+        )
+
+        $lists = [System.Collections.ArrayList]@()
+        [void]$lists.AddRange(@( $groupsList | Select-Object $columns ))
+
+        If ( $lists.count -gt 0 ) {
+
+            Write-Log "Loaded $( $lists.Count ) lists/groups" -severity INFO #-WriteToHostToo $false
+
+        } else {
+
+            $msg = "No lists loaded -> please check!"
+            Write-Log -Message $msg -Severity ERROR
+            throw $msg
+
+        }
+
+        # Return
+        $lists
+
+    }
+
+    end {
+
+    }
+
+}
+
