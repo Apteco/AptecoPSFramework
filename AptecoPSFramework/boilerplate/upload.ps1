e067f77ea45dff250fe129d39707f5947c49c68b
FlorianSf <florian.von.bracht@apteco.de>
2024-07-12 10:59:32 +0200

    Adding more flexibility into the boilerplate to also allow python and pscore handling with job management in #13

--- AptecoPSFramework/boilerplate/upload.ps1
+++ AptecoPSFramework/boilerplate/upload.ps1
@@ -9,13 +9,25 @@
 Param(
 
     [Parameter(Mandatory=$false, ValueFromPipeline=$true, ValueFromPipelineByPropertyName=$true, ParameterSetName='HashtableInput')]
-    [hashtable]$params = [Hashtable]@{},
+    [hashtable]$params = [Hashtable]@{}
 
-    [Parameter(Mandatory=$false, ValueFromPipeline=$true, ValueFromPipelineByPropertyName=$true, ParameterSetName='JsonInput')]
-    [String]$jsonParams = ""
+    #,[Parameter(Mandatory=$false, ValueFromPipeline=$true, ValueFromPipelineByPropertyName=$true, ParameterSetName='JsonInput')]
+    #[String]$JsonParams = ""
+
+    ,[Parameter(Mandatory=$false, ValueFromPipeline=$true, ValueFromPipelineByPropertyName=$true, ParameterSetName='JobIdInput')]
+    [String]$JobId = ""
+
+    ,[Parameter(Mandatory=$false, ValueFromPipelineByPropertyName=$true, ParameterSetName='JobIdInput')]
+    [String]$SettingsFile = ""
+
+
+    ,[Parameter(Mandatory=$false, ValueFromPipelineByPropertyName=$true, ParameterSetName='HashtableInput')]
+    [Parameter(Mandatory=$false, ValueFromPipelineByPropertyName=$true, ParameterSetName='JobIdInput')]
+    [Switch]$DebugMode = $false
 
 )
 
+<#
 # If this script is called by itself, re-transform the escaped json string input back into a hashtable
 If ( $PsCmdlet.ParameterSetName -eq "JsonInput" ) {
     $params = [Hashtable]@{}
@@ -24,13 +36,19 @@ If ( $PsCmdlet.ParameterSetName -eq "JsonInput" ) {
         $params[$_.Name] = $_.Value
     }
 }
-
+#>
 
 #-----------------------------------------------
 # DEBUG SWITCH
 #-----------------------------------------------
 
 $debug = $false
+
+If ( $DebugMode -eq $true ) {
+    $debug = $true
+}
+# TODO make an example lie
+# . ./upload.ps -JobId 123 -DebugMode
 
 
 #-----------------------------------------------
@@ -67,7 +85,9 @@ $Env:Path = ( $scriptPath | Sort-Object -unique ) -join ";"
 # INPUT PARAMETERS, IF DEBUG IS TRUE
 #-----------------------------------------------
 
-if ( $debug -eq $true -and $jsonParams -eq "" ) {
+#if ( $debug -eq $true -and $jsonParams -eq "" ) {
+if ( $debug -eq $true ) {
+
     $params = [hashtable]@{
 
         # Automatic parameters
@@ -87,9 +107,13 @@ if ( $debug -eq $true -and $jsonParams -eq "" ) {
 
         # Integration parameters
         #Force64bit = "true"
-        settingsFile = '.\settings.json'
+        #ForceCore = "true"
+        #ForcePython = "true"
+        #UseJob = "true"
+        settingsFile = '.\inx.yaml'
 
     }
+
 }
 
 #Write-Log -message "Got a file with these arguments: $( [Environment]::GetCommandLineArgs() )" -writeToHostToo $false
@@ -97,103 +121,47 @@ if ( $debug -eq $true -and $jsonParams -eq "" ) {
 
 ################################################
 #
-# NOTES
+# CHECKS
 #
 ################################################
 
-<#
-
-bla bla
-
-#>
-
-
-################################################
-#
-# SCRIPT ROOT
-#
-################################################
-<#
-if ( $debug -eq $true ) {
-
-    if ($MyInvocation.MyCommand.CommandType -eq "ExternalScript") {
-        $scriptPath = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
+If ( $PsCmdlet.ParameterSetName -eq "JobIdInput" ) {
+    If ( $SettingsFile -eq "" ) {
+        throw "Please define a settings file"
     } else {
-        $scriptPath = Split-Path -Parent -Path ([Environment]::GetCommandLineArgs()[0])
+        $settingsfileLocation = $SettingsFile
     }
+} else {
+    $settingsfileLocation = $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath($params.settingsFile)
+}
 
-    $params.scriptPath = $scriptPath
+
+#-----------------------------------------------
+# CHECK IF 64 BIT AND MORE SHOULD BE ENFORCED
+#-----------------------------------------------
+
+$useJob = $false
+
+# Start this if 64 is needed to enforce when this process is 32 bit and system is able to handle it
+$enforce64Bit  = $false
+If ( $params.Force64bit -eq "true" -and [System.Environment]::Is64BitProcess -eq $false -and [System.Environment]::Is64BitOperatingSystem -eq $true ) {
+    $enforce64Bit = $true
+    $useJob = $true
+}
+
+# When you want to use PSCore with 32bit, please change that path in the settings file
+$enforceCore = $false
+If ( $params.ForceCore -eq "true" ) {
+
+    $enforceCore = $true
+    $useJob = $true
 
 }
 
-# Some local settings
-$dir = $params.scriptPath
-Set-Location $dir
-#>
-
-# Set current location to the settings files directory
-$settingsFile = Get-Item $params.settingsFile
-Set-Location $settingsFile.DirectoryName
-
-
-################################################
-#
-# 64 BIT CHECK
-#
-################################################
-
-$thisScript = ".\upload.ps1"
-
-
-#-----------------------------------------------
-# CHECK IF 64 BIT SHOULD BE ENFORCED
-#-----------------------------------------------
-
-# Start this if 64 is needed to enforce when this process is 32 bit and system is able to handle it
-If ( $params.Force64bit -eq "true" -and [System.Environment]::Is64BitProcess -eq $false -and [System.Environment]::Is64BitOperatingSystem -eq $true ) {
-
-    $markerGuid = [guid]::NewGuid().toString()
-
-    try {
-
-        # Input parameter must be a string and for json the double quotes need to be escaped
-        $params.Add("markerGuid", $markerGuid)
-        $paramInput = ( ConvertTo-Json $params -Compress -Depth 99 ).replace('"',"'")
-
-        # This inputs a string into powershell exe at a virtual place "sysnative"
-        # It starts a 64bit version of Windows PowerShell and executes itself with the same input, only encoded as escaped json
-        $j = . $Env:SystemRoot\sysnative\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile -InputFormat text -OutputFormat text -File $thisScript -JsonParams $paramInput -InformationAction "Continue"
-
-    } catch {
-        Exit 1
-    }
-
-    # Check for warnings and errors
-    $j | ForEach-Object {
-        $jrow = $_
-        Switch -Wildcard ( $jrow ) {
-
-            "INFO*" {
-                Write-Information -MessageData $jrow -Tags @("Info") -InformationAction Continue
-            }
-
-            "WARNING*" {
-                Write-Warning -Message $jrow
-            }
-
-            "WARNUNG*" {
-                Write-Warning -Message $jrow
-            }
-
-        }
-    }
-
-    # Convert the PSCustomObject back to a hashtable
-    $markerRow = $j.IndexOf($markerGuid)
-    ( convertfrom-json $j[$markerRow+1].replace("'",'"') ) #.trim()
-
-    Exit 0
-
+$enforcePython = $false
+If ( $params.ForcePython -eq "true" ) {
+    $enforcePython = $true
+    $useJob = $true
 }
 
 
@@ -204,21 +172,23 @@ If ( $params.Force64bit -eq "true" -and [System.Environment]::Is64BitProcess -eq
 ################################################
 
 #-----------------------------------------------
+# CHANGE PATH
+#-----------------------------------------------
+
+# Set current location to the settings files directory
+$settingsFileItem = Get-Item $settingsfileLocation
+Set-Location $settingsFileItem.DirectoryName
+
+
+#-----------------------------------------------
 # IMPORT MODULE
 #-----------------------------------------------
 
 If ($debug -eq $true) {
-    Import-Module "AptecoPSFramework" -Verbose
+    Import-Module "C:\Users\Florian\Documents\GitHub\AptecoPSFramework\AptecoPSFramework" -Verbose
 } else {
-    Import-Module "AptecoPSFramework"
+    Import-Module "C:\Users\Florian\Documents\GitHub\AptecoPSFramework\AptecoPSFramework"
 }
-
-
-#-----------------------------------------------
-# ADD MORE PLUGINS
-#-----------------------------------------------
-
-#Add-PluginFolder "D:\Scripts\CleverReach\Plugins"
 
 
 #-----------------------------------------------
@@ -233,7 +203,41 @@ Set-DebugMode -DebugMode $debug
 #-----------------------------------------------
 
 # Set the settings
-Import-Settings -Path $params.settingsFile
+Import-Settings -Path $settingsfileLocation
+$s = Get-Settings
+
+
+#-----------------------------------------------
+# ADD JOB
+#-----------------------------------------------
+
+If ( $params.UseJob -eq "true" -or $useJob -eq $true ) {
+
+    # Create a new job
+    $jobId = Add-JobLog
+    $jobParams = [Hashtable]@{
+        "JobId" = $JobId
+        #"Plugin" = $script:settings.plugin.guid
+        "InputParam" = $params
+        #"Status" = "Starting"
+        "DebugMode" = $debug
+    }
+    Update-JobLog @jobParams
+
+}
+
+#-----------------------------------------------
+# FIND OUT THE MODE
+#-----------------------------------------------
+
+$mode = "function"
+If ( $enforce64Bit -eq $true ) {
+    $mode = "PSWin64"
+} elseif ( $enforceCore -eq $true ) {
+    $mode = "PSCore"
+} elseif ( $enforcePython -eq $true ) {
+    $mode = "Python"
+}
 
 
 ################################################
@@ -243,43 +247,153 @@ Import-Settings -Path $params.settingsFile
 ################################################
 
 
-# TODO [x] check if we need to make a try catch here -> not needed, if we use a combination like
-
-<#
-            $msg = "Temporary count of $( $mssqlResult ) is less than $( $rowsCount ) in the original export. Please check!"
-            Write-Log -Message $msg -Severity ERROR
-            throw [System.IO.InvalidDataException] $msg
-
-or
-
-            Write-Log -Message "Failed to connect to SQLServer database" -Severity ERROR
-            Write-Log -Message $_.Exception -Severity ERROR
-            throw $_
-
-#>
-
-
 #-----------------------------------------------
 # CALL UPLOAD
 #-----------------------------------------------
+
+$thisScript = ".\upload.ps1"
+
 
 # Added try/catch again because of extras.xml wrapper
 try {
 
     # Do the upload
-    $return = Invoke-Upload $params
+    Switch ( $mode ) {
 
-    # Return the values, if succeeded
-    If ( $PsCmdlet.ParameterSetName -eq "JsonInput" ) {
-        $params.markerGuid  # Output a guid to find out the separator
-        ( ConvertTo-Json $return -Depth 99 -Compress ).replace('"',"'") # output the result as json
-    } else {
-        $return
+        "function" {
+
+            If ( $useJob -eq $true ) {
+                Invoke-Upload -JobId $jobId
+            } else {
+                $return = Invoke-Upload -InputHashtable $params
+            }
+
+            break
+        }
+
+
+        "PSWin64" {
+
+            # This inputs a string into powershell exe at a virtual place "sysnative"
+            # It starts a 64bit version of Windows PowerShell and executes itself with the same input, only encoded as escaped json
+            $j = . $Env:SystemRoot\sysnative\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile -InputFormat text -OutputFormat text -File $thisScript -JobId $jobId -InformationAction "Continue"
+
+            # Check for warnings and errors
+            $j | ForEach-Object {
+                $jrow = $_
+                Switch -Wildcard ( $jrow ) {
+
+                    "INFO*" {
+                        Write-Information -MessageData $jrow -Tags @("Info") -InformationAction Continue
+                    }
+
+                    "WARNING*" {
+                        Write-Warning -Message $jrow
+                    }
+
+                    "WARNUNG*" {
+                        Write-Warning -Message $jrow
+                    }
+
+                }
+            }
+
+            break
+
+        }
+
+
+        "PSCore" {
+
+            # Check if ps core is installed
+            try {
+                $calc = . $s.psCoreExePath { 1+1 }
+                if ( $calc -eq 2 ) {
+                    # Seems to be fine :-)
+                } else {
+                    throw "PowerShell Core does not seem to be installed or found"
+                }
+            } catch {
+                throw "PowerShell Core does not seem to be installed or found"
+            }
+            
+            # This inputs a string into powershell exe at a virtual place "sysnative"
+            # It starts a 64bit version of Windows PowerShell and executes itself with the same input, only encoded as escaped json
+            $j = . $s.psCoreExePath -ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile -InputFormat text -OutputFormat text -File $thisScript -JobId $jobId -InformationAction "Continue"
+
+            # Check for warnings and errors
+            $j | ForEach-Object {
+                $jrow = $_
+                Switch -Wildcard ( $jrow ) {
+
+                    "INFO*" {
+                        Write-Information -MessageData $jrow -Tags @("Info") -InformationAction Continue
+                    }
+
+                    "WARNING*" {
+                        Write-Warning -Message $jrow
+                    }
+
+                    "WARNUNG*" {
+                        Write-Warning -Message $jrow
+                    }
+
+                }
+            }
+
+            break
+        }
+
+
+        "Python" {
+
+            <#
+
+            assuming you have a python file like add.py and this content
+
+            ```Python
+            import sys
+
+            # This program adds two numbers
+            num1 = float(sys.argv[1])
+            num2 = 6.3
+
+            # Add two numbers
+            sum = num1 + num2
+
+            # Display the sum
+            print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+            ```
+
+            #>
+
+            # Then it can be called like this
+            . $s.pythonPath add.py "5.5"
+
+            break
+        }
+
     }
+
+    # return
+    If ( $useJob -eq $true ) {
+        $jobReturn = Get-JobLog -JobId $jobId -ConvertOutputAsHashtable
+        $return = $jobReturn.output
+    }
+
+    $return
+
 
 } catch {
 
     throw $_
     Exit 1
+
+} finally {
+
+    # Close the connection to joblog
+    If ( $useJob -eq $true ) {
+        Close-JobLogDatabase
+    }
 
 }
