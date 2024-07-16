4c2557a7364f2805e8ff3ce5d67322a54b8f3933
FlorianSf <florian.von.bracht@apteco.de>
2024-07-16 14:04:59 +0200

    Implementation of get groups/lists for #13

--- AptecoPSFramework/plugins/InxmailPro/Public/PeopleStage/get-groups.ps1
+++ AptecoPSFramework/plugins/InxmailPro/Public/PeopleStage/get-groups.ps1
@@ -1,20 +1,87 @@
 ﻿
 
 function Get-Groups {
-    [CmdletBinding()]
+
+    [CmdletBinding(DefaultParameterSetName = 'Object')]
     param (
-        [Parameter(Mandatory=$false)][Hashtable] $InputHashtable
-        #[Parameter(Mandatory=$false)][Switch] $DebugMode = $false
+        [Parameter(Mandatory=$true, ParameterSetName = 'Object')][Hashtable]$InputHashtable        # This creates a new entry in joblog
+        ,[Parameter(Mandatory=$true, ParameterSetName = 'Job')][Int]$JobId                          # This uses an existing joblog entry
     )
 
     begin {
 
 
         #-----------------------------------------------
-        # LOG
+        # MODULE INIT
         #-----------------------------------------------
 
-        $moduleName = "GETGROUPS"
+        $moduleName = "GROUPS"
+
+
+        #-----------------------------------------------
+        # START TIMER
+        #-----------------------------------------------
+
+        $processStart = [datetime]::now
+
+        write-log "Parameterset: $( $PSCmdlet.ParameterSetName )"
+
+
+        #-----------------------------------------------
+        # CHECK INPUT AND SET JOBLOG
+        #-----------------------------------------------
+
+        # Log the job in the database
+        Set-JobLogDatabase
+
+        Switch ( $PSCmdlet.ParameterSetName ) {
+
+            "Object" {
+                Write-log "adding a new job in function"
+
+                # Create a new job
+                $JobId = Add-JobLog
+                $jobParams = [Hashtable]@{
+                    "JobId" = $JobId
+                    "Plugin" = $script:settings.plugin.guid
+                    "InputParam" = $InputHashtable
+                    "Status" = "Starting"
+                    "DebugMode" = $Script:debugMode
+                    "Type" = $moduleName
+                }
+                Update-JobLog @jobParams
+
+                break
+            }
+
+            "Job" {
+                Write-log "updating existing job"
+
+                # Get the jobs information
+                $job = Get-JobLog -JobId $JobId -ConvertInput
+                $InputHashtable = $job.input
+
+                # Update the job with more information
+                $jobParams = [Hashtable]@{
+                    "JobId" = $JobId
+                    "Plugin" = $script:settings.plugin.guid
+                    "Status" = "Starting"
+                    "Type" = $moduleName
+                }
+                Update-JobLog @jobParams
+
+                # Set the current process id
+                Set-ProcessId -Id $job.process
+
+                break
+            }
+
+        }
+
+
+        #-----------------------------------------------
+        # LOG
+        #-----------------------------------------------
 
         # Start the log
         Write-Log -message $Script:logDivider
@@ -30,21 +97,24 @@ function Get-Groups {
         }
 
         #-----------------------------------------------
-        # DEPENDENCIES
+        # DEBUG MODE
         #-----------------------------------------------
 
-        #Import-Module MeasureRows
-        #Import-Module SqlServer
-        #Import-Module ConvertUnixTimestamp
-        #Import-Lib -IgnorePackageStructure
+        Write-Log "Debug Mode: $( $Script:debugMode )"
+
+
+        #-----------------------------------------------
+        # OPEN DEFAULT DUCKDB CONNECTION (NOT JOBLOG)
+        #-----------------------------------------------
+
+        #Open-DuckDBConnection
+
 
     }
 
     process {
 
-        # Load mailings data from CleverReach
         $groups = Get-List -Type STANDARD
-
         Write-Log "Loaded $( $groups.Count ) groups from Inxmail" -severity INFO
 
         # Load and filter list into array of mailings objects
@@ -86,8 +156,49 @@ function Get-Groups {
 
         }
 
-        # Return
-        $lists
+
+        #-----------------------------------------------
+        # STOP TIMER
+        #-----------------------------------------------
+
+        $processEnd = [datetime]::now
+        $processDuration = New-TimeSpan -Start $processStart -End $processEnd
+        Write-Log -Message "Needed $( [int]$processDuration.TotalSeconds ) seconds in total"
+
+        
+        #-----------------------------------------------
+        # CLOSE DEFAULT DUCKDB CONNECTION
+        #-----------------------------------------------
+
+        #Close-DuckDBConnection
+
+
+        #-----------------------------------------------
+        # RETURN VALUES TO PEOPLESTAGE
+        #-----------------------------------------------
+
+        # log the return into database and close connection
+        $jobReturnParams = [Hashtable]@{
+            "JobId" = $JobId
+            "Status" = "Finished"
+            "Finished" = $true
+            "Successful" = $lists.Count
+            "Failed" = 0 # TODO needs correction
+            "Totalseconds" = $processDuration.TotalSeconds
+            "OutputArray" = $lists
+        }
+        Update-JobLog @jobReturnParams
+        Close-JobLogDatabase
+
+
+        # return the results
+        Switch ( $PSCmdlet.ParameterSetName ) {
+            "Object" {
+                $lists
+                break
+            }
+            # Otherwise the results are now in the database
+        }
 
     }
 
