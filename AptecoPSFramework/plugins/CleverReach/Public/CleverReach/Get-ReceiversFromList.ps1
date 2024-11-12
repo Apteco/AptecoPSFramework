8b6f3894cd627cb5f8329c47b60facb231af3fb6
Florian von Bracht <github@ritterstation.de>
2024-11-12 12:16:46 +0000

    CleverReach: Download all receivers of a list

---
+++ AptecoPSFramework/plugins/CleverReach/Public/CleverReach/Get-ReceiversFromList.ps1
@@ -0,0 +1,41 @@
+﻿# Source is the first part of the Tag, so apteco.a1qhvh3_20230607201732 split in "apteco" as source and "a1qhvh3_20230607201732" as tag
+
+function Get-ReceiversFromList {
+
+    [CmdletBinding()]
+    param (
+        #[Parameter(Mandatory=$false)][Hashtable] $InputHashtable
+         [Parameter(Mandatory=$true)][Int] $ListId
+        ,[Parameter(Mandatory=$false)][Int] $Detail = 0
+    )
+
+    begin {
+
+    }
+    process {
+
+        # TODO possibly use array rather than single tag
+
+        # Prepare inactives query as security net
+        $filterBody = [PSCustomObject]@{
+            "groups" = [Array]@(,$ListId)
+            "operator" = "AND"
+            "rules" = [Array]@()
+            "orderby" = "activated desc"
+            "detail" = $Detail
+        }
+
+        $receiversWithTag = [Array]@( Invoke-CR -Object "receivers" -Path "filter.json" -Method POST -Paging -Body $filterBody ) # .PsObject.Copy() use a copy so the reference is not changed because it will used a second time
+
+        $receiversWithTag
+
+    }
+
+    end {
+
+    }
+
+}
+
+
+
