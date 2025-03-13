473b3c7a956d278e0a0ba74f7b29fd6a20c0deb2
FlorianSf <florian.von.bracht@apteco.de>
2025-03-13 16:55:15 +0100

    Added a bulk option for Invoke-SFSCQuery, so you are not limited to 2000 records in #15

--- AptecoPSFramework/plugins/SalesforceSC/Public/SalesforceSC/Invoke-SFSCQuery.ps1
+++ AptecoPSFramework/plugins/SalesforceSC/Public/SalesforceSC/Invoke-SFSCQuery.ps1
@@ -9,7 +9,7 @@ function Invoke-SFSCQuery {
         ,[Parameter(Mandatory=$false)][Switch] $Bulk = $false    # TODO implement, use this one to use a bulk select instead of a direct query
         ,[Parameter(Mandatory=$false)][Switch] $IncludeAttributes = $false    # TODO implement, use this one to use a bulk select instead of a direct query
         ,[Parameter(Mandatory=$false)][Switch] $ReturnCount = $false    # Returns a count instead of the records
-
+        ,[Parameter(Mandatory=$false)][Switch] $QueryAll = $false    # Only for Bulk to also include deleted records
     )
 
     begin {
@@ -21,11 +21,23 @@ function Invoke-SFSCQuery {
             "q" = $Query
         }
 
+        # TODO [ ] Maybe turn this off because of multiline queries
         Write-Log -severity verbose -Message "Executing query:"
         Write-Log -severity verbose -Message "  $( $Query )"
 
         If ( $Bulk -eq $true ) {
-            # TODO [ ] implement
+            # TODO [x] implement
+            $bulkParams = [Hashtable]@{
+                "Query" = $Query                
+                "Path" = ".\newData.csv" 
+            }
+            If ( $QueryAll -eq $True ) {
+                $bulkParams.Add("QueryOperation", "queryAll")
+            } else {
+                $bulkParams.Add("QueryOperation", "query")
+            }
+            $return = Add-BulkJob @bulkParams
+
         } else {
             $result = Invoke-SFSC -Service "data" -Object "query" -Query $queryObj -Method "Get"
             <#
@@ -44,27 +56,29 @@ function Invoke-SFSCQuery {
                 )
             }
             #>
-        }
+        
 
-        #$objects = Invoke-RestMethod -URI "$( $base )/services/data/v$( $version )/sobjects/" -Method Get -verbose -ContentType $contentType -Headers $headers
+            #$objects = Invoke-RestMethod -URI "$( $base )/services/data/v$( $version )/sobjects/" -Method Get -verbose -ContentType $contentType -Headers $headers
 
-        #return
-        Write-Log -severity verbose -Message "Result:"
-        Write-Log -severity verbose -Message "  Status: $( $result.done ) "
-        Write-Log -severity verbose -Message "  Records: $( $result.totalSize ) "
+            #return
+            Write-Log -severity verbose -Message "Result:"
+            Write-Log -severity verbose -Message "  Status: $( $result.done ) "
+            Write-Log -severity verbose -Message "  Records: $( $result.totalSize ) "
 
-        #$result #| where-object { $_.createable -eq $true } | Select-Object name, label | Out-GridView -PassThru
+            #$result #| where-object { $_.createable -eq $true } | Select-Object name, label | Out-GridView -PassThru
 
-        # return
-        $return = $null
-        If ( $ReturnCount -eq $true ) {
-            $return = $result.totalSize
-        } else {
-            If ( $IncludeAttributes -eq $true ) {
-                $return = $result.records
+            # return
+            $return = $null
+            If ( $ReturnCount -eq $true ) {
+                $return = $result.totalSize
             } else {
-                $return = $result.records | Select-Object * -ExcludeProperty attributes
+                If ( $IncludeAttributes -eq $true ) {
+                    $return = $result.records
+                } else {
+                    $return = $result.records | Select-Object * -ExcludeProperty attributes
+                }
             }
+
         }
 
         $return
