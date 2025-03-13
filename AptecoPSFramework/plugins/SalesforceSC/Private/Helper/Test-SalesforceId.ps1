0ef4c9e4195cf940212d9fec776c77f06ce50457
FlorianSf <florian.von.bracht@apteco.de>
2025-03-13 16:17:23 +0100

    Adding a function to find out if the current ID is a salesforce ID or not for #15
    It is pretty basic in the moment, checking the instance ID and the length of the ID.

---
+++ AptecoPSFramework/plugins/SalesforceSC/Private/Helper/Test-SalesforceId.ps1
@@ -0,0 +1,31 @@
+﻿
+
+function Test-SalesforceId {
+
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true)][String] $SalesforceId
+    )
+
+    process {
+
+        #-----------------------------------------------
+        # CHECK THE ID
+        #-----------------------------------------------
+
+        If ( $SalesforceId.Length -eq 18 ) {
+            If ( $SalesforceId.Substring(3,3) -eq $Script:settings.instanceId ) {
+                $true
+            } else {
+                $false
+            }
+        } else {
+            $false
+        }
+
+    }
+
+}
+
+
+
