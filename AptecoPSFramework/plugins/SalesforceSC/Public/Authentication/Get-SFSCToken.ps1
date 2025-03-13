32b6b80e36319daa41f8f530c79aba3318297b91
FlorianSf <florian.von.bracht@apteco.de>
2025-03-13 17:12:38 +0100

    Adding more documentation and synopsis for the salesforce functions

--- AptecoPSFramework/plugins/SalesforceSC/Public/Authentication/Get-SFSCToken.ps1
+++ AptecoPSFramework/plugins/SalesforceSC/Public/Authentication/Get-SFSCToken.ps1
@@ -15,12 +15,6 @@ function Get-SFSCToken {
     process {
 
         $oAuthIntrospect = Invoke-SFSC -Service "oauth2" -Object "introspect" -Method "Post"
-
-        # $objects = Invoke-SFSC -Object "sobjects" -Method "Get"
-
-        # #$objects = Invoke-RestMethod -URI "$( $base )/services/data/v$( $version )/sobjects/" -Method Get -verbose -ContentType $contentType -Headers $headers
-        # $obj = $objects.sobjects | where { $_.createable -eq $true } | Select-Object name, label | Out-GridView -PassThru
-
         $oAuthIntrospect
 
     }
