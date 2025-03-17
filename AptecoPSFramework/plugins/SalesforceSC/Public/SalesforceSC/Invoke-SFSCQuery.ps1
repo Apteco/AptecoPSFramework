c874c94ef0eba0231b9c6066103a28f3a1e41dd7
FlorianSf <florian.von.bracht@apteco.de>
2025-03-17 14:00:03 +0100

    Overworked the Invoke-SFSCQuery with Synopsis and some improvements in logging and writing out the result to a file
    The synopsis is AI generated...

--- AptecoPSFramework/plugins/SalesforceSC/Public/SalesforceSC/Invoke-SFSCQuery.ps1
+++ AptecoPSFramework/plugins/SalesforceSC/Public/SalesforceSC/Invoke-SFSCQuery.ps1
@@ -2,14 +2,67 @@
 
 function Invoke-SFSCQuery {
 
-    [CmdletBinding()]
+<#
+    .SYNOPSIS
+        Executes a SOQL query against Salesforce.
+
+    .DESCRIPTION
+        This function executes a SOQL query against Salesforce using either the direct query method or the bulk query method.
+        It allows specifying whether to include attributes in the result, return the count of records, or use the queryAll option for bulk queries.
+
+    .PARAMETER Query
+        The SOQL query to execute.
+
+    .PARAMETER Bulk
+        Switch to use bulk query instead of a direct query.
+
+    .PARAMETER IncludeAttributes
+        Switch to include attributes in the result for direct queries.
+
+    .PARAMETER ReturnCount
+        Switch to return the count of records instead of the records themselves for direct queries.
+
+    .PARAMETER QueryAll
+        Switch to use the queryAll option for bulk queries.
+
+    .EXAMPLE
+        Invoke-SFSCQuery -Query "SELECT Id, Name FROM Account" -IncludeAttributes
+
+    .EXAMPLE
+        Invoke-SFSCQuery -Query "SELECT Id FROM Contact WHERE LastName LIKE 'A%'" -ReturnCount
+
+    .EXAMPLE
+        Invoke-SFSCQuery -Query "SELECT Id, Name FROM Lead" -Bulk -QueryAll
+
+    .INPUTS
+        None. You cannot pipe objects to this function.
+
+    .OUTPUTS
+        System.Object. The function returns the result of the SOQL query.
+
+    .NOTES
+        Author: florian.von.bracht@apteco.de
+#>
+
+    [CmdletBinding(DefaultParameterSetName = 'Direct')]
     param (
-        #[Parameter(Mandatory=$false)][Hashtable] $InputHashtable
-        [Parameter(Mandatory=$true)][String] $Query
-        ,[Parameter(Mandatory=$false)][Switch] $Bulk = $false    # TODO implement, use this one to use a bulk select instead of a direct query
-        ,[Parameter(Mandatory=$false)][Switch] $IncludeAttributes = $false    # TODO implement, use this one to use a bulk select instead of a direct query
-        ,[Parameter(Mandatory=$false)][Switch] $ReturnCount = $false    # Returns a count instead of the records
-        ,[Parameter(Mandatory=$false)][Switch] $QueryAll = $false    # Only for Bulk to also include deleted records
+
+         [Parameter(Mandatory=$True, ParameterSetName = 'Direct')]
+         [Parameter(Mandatory=$True, ParameterSetName = 'Bulk')]
+         [String]$Query
+
+        ,[Parameter(Mandatory=$False, ParameterSetName = 'Bulk')]
+         [Switch]$Bulk = $false
+
+        ,[Parameter(Mandatory=$False, ParameterSetName = 'Direct')]
+         [Switch]$IncludeAttributes = $false
+
+        ,[Parameter(Mandatory=$False, ParameterSetName = 'Direct')]
+         [Switch]$ReturnCount = $false
+
+        ,[Parameter(Mandatory=$False, ParameterSetName = 'Bulk')]
+         [Switch]$QueryAll = $false
+
     )
 
     begin {
@@ -21,15 +74,18 @@ function Invoke-SFSCQuery {
             "q" = $Query
         }
 
-        # TODO [ ] Maybe turn this off because of multiline queries
         Write-Log -severity verbose -Message "Executing query:"
-        Write-Log -severity verbose -Message "  $( $Query )"
+        Write-Log -severity verbose -Message "  $( $Query.replace("`r`n"," ").replace("`n"," ").replace("`r"," ") )"
 
         If ( $Bulk -eq $true ) {
-            # TODO [x] implement
+
+            $bulkPath = Join-Path -Path $Env:tmp -ChildPath "$( [guid]::newguid().ToString() ).csv"
+
+            Write-Log "Writing results to '$( $bulkPath )'"
+
             $bulkParams = [Hashtable]@{
                 "Query" = $Query                
-                "Path" = ".\newData.csv" 
+                "Path" = $bulkPath
             }
             If ( $QueryAll -eq $True ) {
                 $bulkParams.Add("QueryOperation", "queryAll")
@@ -39,6 +95,7 @@ function Invoke-SFSCQuery {
             $return = Add-BulkJob @bulkParams
 
         } else {
+
             $result = Invoke-SFSC -Service "data" -Object "query" -Query $queryObj -Method "Get"
             <#
             $result = [PSCustomObject]@{
